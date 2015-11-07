5becf9a2e901b18eda599a5fa97e109de5d28ee0
Jerome Rasky <jyrome.112@gmail.com>
2015-11-07 11:41:50 -0800

    Added license headers

--- kernel.c
+++ kernel.c
@@ -1,5 +1,12 @@
 /*
  * kernel.c
+ *
+ * Copyright (C) 2015 Jerome Rasky
+ *
+ * Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+ * http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT
+ * or http://opensource.org/licenses/MIT>, at your option. This file may not be
+ * copied, modified, or distributed except according to those terms.
  */
 #include <stddef.h>
 #include <stdint.h>

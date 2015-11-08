d111c2e0d9cb983e7ab879723f80e0241ceaafd0
Jerome Rasky <jyrome.112@gmail.com>
2015-11-08 11:35:50 -0800

    Accept arguments to kmain
    Magic number argument indicating bootloader, as well as boot info
    pointer
    
    TODO: Process the boot info header

--- src/kernel.c
+++ src/kernel.c
@@ -18,18 +18,28 @@ static const char str[] = "my first kernel";
 static const size_t str_size = sizeof(str) - 1;
 static char *vidptr = (char*)0xb8000; // video memory begins here
 
-void kmain(void) {
-  // write str and clear the screen
-  for (size_t i = 0; i < TERM_ROWS * TERM_LINES; i++) {
-    if (i < str_size) {
-      // write the character from the string
-      vidptr[i * 2] = str[i];
-    } else {
-      // clear the character
-      vidptr[i * 2] = ' ';
-    }
+struct boot_info_header {
+};
 
-    // set the color
-    vidptr[i * 2 + 1] = 0x07;
+void kmain(uint32_t magic, void *boot_info) {
+  if (magic == 0x2BADB002) {
+    // multiboot 1
+
+    // write str and clear the screen
+    for (size_t i = 0; i < TERM_ROWS * TERM_LINES; i++) {
+      if (i < str_size) {
+        // write the character from the string
+        vidptr[i * 2] = str[i];
+      } else {
+        // clear the character
+        vidptr[i * 2] = ' ';
+      }
+
+      // set the color
+      vidptr[i * 2 + 1] = 0x07;
+    }
+  } else if (magic == 0x36d76289) {
+    // multiboot 2
+
   }
 }

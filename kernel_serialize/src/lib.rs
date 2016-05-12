d0eb34507dac747cc7d68cff2bc40c5bc8b50ba6
Jerome Rasky <jyrome.112@gmail.com>
2016-05-12 12:52:25 -0700

    WIP do more work

--- kernel_serialize/src/lib.rs
+++ kernel_serialize/src/lib.rs
@@ -14,56 +14,18 @@ use uuid::Uuid;
 
 mod executable;
 
-#[derive(Debug, Clone, Copy)]
-pub enum ResourceError {
-    NoRequest
+pub struct Resource<T: ResourceData> {
+    ty: Uuid,
+    id: Uuid,
+    data: Arc<RefCell<T>>
 }
 
-#[derive(Debug, Clone, Copy)]
-pub enum SerializerError {
-    NotSerializable,
-    InvalidData,
-    Store(StoreError)
-}
-
-pub trait Serializer {
-    fn write(&mut self, buf: &[u8]) -> Result<(), SerializerError>;
-}
-
-pub trait Deserializer {
-    fn read(&mut self, buf: &mut [u8]) -> Result<(), SerializerError>;
-}
-
-#[derive(Debug, Clone, Copy)]
-pub enum StoreError {
-    IdCollision,
-    StoreFull,
-    ReadOnly,
-    Disabled,
-    NotFound,
-    InUse
-}
-
-pub trait Store: Resource {
-    fn has(&self, id: Uuid) -> bool;
-    fn get(&self, id: Uuid) -> Result<&Resource, StoreError>;
-    fn get_mut(&mut self, id: Uuid) -> Result<&mut Resource, StoreError>;
-    fn remove(&mut self, id: Uuid) -> Result<Resource, StoreError>;
-    fn insert(&mut self, resource: Resource) -> Result<Uuid, StoreError>;
-}
-
-pub trait Resource: Any {
-    fn ty(&self) -> Uuid;
-
-    fn id(&self) -> Option<Uuid> {
-        None
-    }
-
+pub trait ResourceData: Any {
     fn serialize(&self, ser: &mut Serializer) -> Result<(), SerializerError> {
         Err(SerializerError::NotSerializable)
     }
 
-    fn deserialize(id: Uuid, de: &mut Deserializer) -> Result<Self, SerializerError> {
+    fn deserialize(de: &mut Deserializer) -> Result<Self, SerializerError> {
         Err(SerializerError::NotSerializable)
     }
 }

c27211dcbe0cbeb352ffd371bcda83cf4a7b9502
Jerome Rasky <jyrome.112@gmail.com>
2017-01-18 11:45:21 -0800

    Clean up dead code

--- log_abi/src/include.rs
+++
@@ -1,8 +0,0 @@
-pub use std::fmt::Display;
-pub use std::sync::atomic::{AtomicUsize, Ordering};
-pub use std::raw::TraitObject;
-pub use std::marker::{Sync, Send};
-
-pub use std::mem;
-
-pub use spin::Mutex;
